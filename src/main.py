8a08987d16025c6342c5705d098e28d6034d5796
Tyler <tyler2ber@gmail.com>
2026-07-10 12:55:19 -0400

    Added rename and resize functionality

--- src/main.py
+++ src/main.py
@@ -1,56 +1,58 @@
 from PIL import Image
-from pathlib import Path
+import os
 
 
 print("\n---- IMAGEFOLDER EDITOR ----")
 print("provide the images, get them renamed and/or resized\n\n")
 
-folder_path = Path("imagefolder-original")
-extensions = [".jpg", ".jpeg", ".png"]
+folderpath_original = "imagefolder-original"
+folderpath_new = "imagefolder-new"
 
-# loop through imagefolder-original
-for filepath in folder_path.iterdir():
-	if filepath.suffix in extensions:
+for file in os.listdir(folderpath_original):
+	if file.split(".")[1] in ["jpg", "jpeg", "png"]:
 
-		print(f"current_filepath: {filepath}")
+		# image
 
-		with Image.open(filepath) as image:
+		image = Image.open(f"{folderpath_original}/{file}")
 
-			image_name = image.filename.split('/')[1]
-			image_name_new = image_name # set to change
+		image_name = image.filename.split("/")[1]
+		image_width, image_height = image.size
 
-			image_width, image_height = image.size
-			image_width_new = image_width # set to change
+		print(f"current: {folderpath_original}/{file}")
+		print(f"====> {image_name}({image_width}x{image_height})")
 
-			print(f"====> {image_name} ({image_width}x{image_height})")
+		# image_new
 
-			# ask about rename
-			while True:
-				should_rename_image = input("rename image? (y/n): ")
-				if should_rename_image == "y":
-					print("RENAMING...")
-					image_name_new = input(f"Enter new name for {image_name}: ") + "." + image.filename.split(".")[1]
-					break
-				elif should_rename_image == "n":
-					print("skipping rename...")
-					break
-				else:
-					print("ERROR: enter 'y' or 'n'")
-			print(f"CONFIRMED: {image_name} ==> {image_name_new}")
+		image_name_new = image_name # set to change
+		image_width_new = image_width # set to change
 
-			# ask about resize
-			while True:
-				should_resize_image = input("resize image? (y/n): ")
-				if should_resize_image == "y":
-					print("RESIZING...")
-					image_width_new = input(f"Enter new width for {image_name}({image_width}xheight): ")
-					break
-				elif should_resize_image == "n":
-					print("skipping resize...")
-					break
-				else:
-					print("ERROR: enter 'y' or 'n'")
-			print(f"CONFIRMED: {image_width} ==> {image_width_new}")
+		# RENAMING
+		while True:
+			should_rename = input("rename image? (y/n): ")
+			if should_rename == "y":
+				print("RENAMING...")
+				image_name_new = input(f"enter new name for {image_name}: ") + "." + image_name.split(".")[1]
+				break
+			elif should_rename == "n":
+				print("skipping rename...")
+				break
+			else:
+				print("ERROR: enter 'y' or 'n'")
 
-			# rename and/or resize the image
-			print("[insert_done_message]\n\n")
+		# RESIZING
+		while True:
+			should_resize = input("resize image? (y/n): ")
+			if should_resize == "y":
+				print("RESIZING...")
+				image_width_new = int(input(f"enter new width for {image_name}({image_width}x{image_height}): "))
+				image_height = int((image_width_new/image_width) * image_height)
+				break
+			elif should_resize == "n":
+				print("skipping resize...")
+				break
+			else:
+				print("ERROR: enter 'y' or 'n'")
+
+		image_new = image.resize((image_width_new, image_height))
+		image_new.save(f"{folderpath_new}/{image_name_new}")
+		print(f"====> {image_name_new}({image_width_new}x{image_height})\n")
